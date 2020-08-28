f4f7627479438659e9f7ce3a5dff25c9557a114c
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 20:30:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,3 +3,8 @@ def position_taken?(board, index)
   ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
   end
 end
+puts position_taken?(ttt_board, 0)
+puts position_taken?(ttt_board, 1)
+puts position_taken?(ttt_board, 2)
+puts position_taken?(ttt_board, 3)
+puts position_taken?(ttt_board, 4)
